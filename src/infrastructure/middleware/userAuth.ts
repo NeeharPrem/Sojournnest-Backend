2bfb572001a7d2bf4e1d52d781d9194118f89a57
NeeharPrem <neeharprem94@gmail.com>
2024-02-03 19:46:04 +0530

    Enhanced user authentication middleware to support new access token and refresh token mechanisms

--- src/infrastructure/middleware/userAuth.ts
+++ src/infrastructure/middleware/userAuth.ts
@@ -1,9 +1,8 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import { Request, Response,NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import userRepository from '../repository/userRepository';
 
-const userRepo= new userRepository()
-
+const userRepo = new userRepository();
 
 declare global {
     namespace Express {
@@ -15,23 +14,34 @@ declare global {
 
 export const protect = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        let token = req.cookies.userJWT;
+        const token = req.cookies.userJWT;
 
         if (!token) {
+            console.log('here')
             return res.status(401).json({ message: "Access Denied" });
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
+        console.log(decoded)
         if (!decoded || (decoded.role && decoded.role !== 'user')) {
             return res.status(401).json({ message: 'Not authorized, invalid token' });
         }
+
+        const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+        if (decoded.exp && decoded.exp < currentTimeInSeconds) {
+            return res.status(401).json({ message: 'Token has expired' });
+        }
+
         const user = await userRepo.findById(decoded.userId as string);
         if (!user) {
             return res.status(401).json({ message: 'Not authorized, invalid token' });
         }
+
         req.userId = user._id;
         if (user.is_blocked) {
             return res.status(401).json({ message: 'You are blocked by admin!' });
         }
+
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Not authorized, invalid token' });
