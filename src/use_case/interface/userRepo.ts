88ff8c422c9aa2e2d2d810a4209a93cfd3439a34
NeeharPrem <neeharprem94@gmail.com>
2024-02-14 18:51:05 +0530

    interface for saving refresh token added

--- src/use_case/interface/userRepo.ts
+++ src/use_case/interface/userRepo.ts
@@ -5,6 +5,7 @@ interface IUserRepo {
     findByEmail(email: string): Promise<User | null>;
     findById(_id: string): Promise<User | null>;
     findOneAndUpdate(query: any, update: any, options?: any): Promise<User | null>;
+    updateRefreshToken(_id: string, refreshToken: string): Promise<User | null>;
     findAndUpdate(user: User): Promise<any>
 }
 
