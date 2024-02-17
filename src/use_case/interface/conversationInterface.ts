ea35d455854df132a890ead09e8773ccfea4f9fe
NeeharPrem <neeharprem94@gmail.com>
2024-02-17 08:45:33 +0530

    Added interface for updating the lastseen

--- src/use_case/interface/conversationInterface.ts
+++ src/use_case/interface/conversationInterface.ts
@@ -4,6 +4,7 @@ interface Conversation {
 }
 export default interface IConversationRepoInterface {
     save(conversation: Array<string>): Promise<any>,
+    updateUserLastSeen(id:string,data:Date):Promise<any>,
     findByUserId(id: string): Promise<any>
     checkExisting(members: Array<string>): Promise<any>
 }
