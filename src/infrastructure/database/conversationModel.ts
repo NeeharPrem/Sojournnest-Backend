1979da1afdaef5f8bd08f13e2ef5ae4112597661
NeeharPrem <neeharprem94@gmail.com>
2024-02-17 08:59:52 +0530

    Model structure updated to store lastseen of each user in the database

--- src/infrastructure/database/conversationModel.ts
+++ src/infrastructure/database/conversationModel.ts
@@ -1,12 +1,28 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+interface Member {
+    userId: string;
+    lastSeen: Date;
+}
+
 interface Conversation extends Document {
-    members: Array<string>;
+    members: Array<Member>;
 }
 
 const ConversationSchema = new Schema<Conversation>({
     members: {
-        type: [String],
+        type: [
+            {
+                userId: {
+                    type: String,
+                },
+                lastSeen: {
+                    type: Date,
+                    default: Date.now
+                }
+            }
+        ],
+        required: true
     }
 }, { timestamps: true });
 
