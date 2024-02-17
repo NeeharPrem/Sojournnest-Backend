f5f48b5c1aa3a243707a4a4f1e15a73b36c394ce
NeeharPrem <neeharprem94@gmail.com>
2024-02-17 09:06:10 +0530

    Interface for sort,search,filter implimented

--- src/use_case/interface/hostRepo.ts
+++ src/use_case/interface/hostRepo.ts
@@ -2,6 +2,7 @@ import Room from "../../domain/room";
 
 interface IHostRepo{
     addnewRoom(roomData:Room): Promise<any>;
+    findAll(query: any, sortOptions:string):Promise<any>;
     findListings():Promise<any>;
     findById(_id: string): Promise<Room | null>;
     getListings(_id: string): Promise<Room[]>;
