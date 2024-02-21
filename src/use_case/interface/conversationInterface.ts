7cd304bad49a25219db432df25804af80e279f10
NeeharPrem <neeharprem94@gmail.com>
2024-02-21 14:40:04 +0530

    Interfaces updated to handle the new data which is passed to the repo

--- src/use_case/interface/conversationInterface.ts
+++ src/use_case/interface/conversationInterface.ts
@@ -3,8 +3,8 @@ interface Conversation {
 
 }
 export default interface IConversationRepoInterface {
-    save(conversation: Array<string>): Promise<any>,
+    save(conversation: Array<{ memberId: string }>, senderId:string): Promise<any>,
     updateUserLastSeen(id:string,data:Date):Promise<any>,
     findByUserId(id: string): Promise<any>
-    checkExisting(members: Array<string>): Promise<any>
+    checkExisting(members: Array<{ memberId: string }>, senderId:string): Promise<any>
 }
