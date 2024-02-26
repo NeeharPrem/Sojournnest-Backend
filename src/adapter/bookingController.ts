fe9970f0751a652dd5891406c6f29763fe87429d
NeeharPrem <neeharprem94@gmail.com>
2024-02-26 19:37:46 +0530

    feat: bookingController

--- src/adapter/bookingController.ts
+++ src/adapter/bookingController.ts
@@ -15,6 +15,37 @@ class BookingController{
             console.log(error)
         }
     }
+
+    async getBookingdate(req: Request, res: Response) {
+        try {
+            const roomId= req.params.id
+            const Data = await this.BookingUsecase.getBookingdate(roomId)
+            return res.status(Data?.status || 400).json(Data?.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async checkDateAvailability(req: Request, res: Response) {
+        try {
+            const { roomId, checkInDate, checkOutDate } = req.body;
+
+            const convertDate = (dateString: string) => {
+                const [day, month, year] = dateString.split('/');
+                return new Date(Date.UTC(+year, +month - 1, +day));
+            };
+            const checkInISO = convertDate(checkInDate).toISOString();
+            const checkOutISO = convertDate(checkOutDate).toISOString();
+            const Data = await this.BookingUsecase.checkDateAvailability(roomId, checkInISO, checkOutISO);
+            if(Data){
+                return res.status(Data.status).json(Data?.data)
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: 'An error occurred while checking date availability.' });
+        }
+    }
+
 }
 
 export default BookingController
