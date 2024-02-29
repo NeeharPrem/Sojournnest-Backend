d08d95aa388c9bb3359073251652490806708a7e
NeeharPrem <neeharprem94@gmail.com>
2024-02-29 10:10:12 +0530

    feat: added interfaces finding booking by and updating

--- src/use_case/interface/bookingRepoInterface.ts
+++ src/use_case/interface/bookingRepoInterface.ts
@@ -1,7 +1,10 @@
 import Booking from "../../domain/booking"
 
 interface IBookingRepo{
-    addnewBooking(Data: Booking):Promise<any>
+    addnewBooking(Data: any):Promise<any>
+    getBookings(Id:string):Promise<any>
+    getBookingById(Id:string,bookingId:string):Promise<any>
+    updateBookingStatus(bookingId: string, newstatus:string):Promise<any>
     getBookingdate(id:string):Promise<any>
     checkDateAvailability(roomId: string, checkInISO: string, checkOutISO:string):Promise<any>
 }
