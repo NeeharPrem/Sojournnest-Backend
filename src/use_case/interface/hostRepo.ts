caec8d31ec76359d99e30e19e1bdae34f573efee
NeeharPrem <neeharprem94@gmail.com>
2024-03-02 15:33:45 +0530

    fixed the type that accepted by the findlisting repo

--- src/use_case/interface/hostRepo.ts
+++ src/use_case/interface/hostRepo.ts
@@ -3,7 +3,7 @@ import Room from "../../domain/room";
 interface IHostRepo{
     addnewRoom(roomData:Room): Promise<any>;
     findAll(query: any, sortOptions:string):Promise<any>;
-    findListings():Promise<any>;
+    findListings(page:number):Promise<any>;
     findById(_id: string): Promise<Room | null>;
     getListings(_id: string): Promise<Room[]>;
     findOneAndUpdate(query: any, update: any, options?: any): Promise<Room | null>; 
