855398be6621259a79a0cf4fbde94f34df3b7702
NeeharPrem <neeharprem94@gmail.com>
2024-03-11 21:13:22 +0530

    update: updated repository for host repository in favor of host repository

--- src/use_case/interface/hostRepo.ts
+++ src/use_case/interface/hostRepo.ts
@@ -6,7 +6,10 @@ interface IHostRepo{
     findListings(page:number):Promise<any>;
     findById(_id: string): Promise<Room | null>;
     getListings(_id: string): Promise<Room[]>;
-    findOneAndUpdate(query: any, update: any, options?: any): Promise<Room | null>; 
+    findOneAndUpdate(query: any, update: any, options?: any): Promise<Room | null>;
+    blockDate(roomId:string,data:object):Promise<any>
+    blockedDates(roomId:string):Promise<any>;
+    removeDate(roomId:string,data:object):Promise<any>;
 }
 
 export default IHostRepo;
