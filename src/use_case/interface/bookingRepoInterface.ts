d512d45a50450548fca910504d0ad1fc37075b19
NeeharPrem <neeharprem94@gmail.com>
2024-03-15 13:59:02 +0530

    feat: added interface for the booking repository for better handling or logic and typesafety

--- src/use_case/interface/bookingRepoInterface.ts
+++ src/use_case/interface/bookingRepoInterface.ts
@@ -3,6 +3,9 @@ import Booking from "../../domain/booking"
 interface IBookingRepo{
     addnewBooking(Data: any):Promise<any>
     getBookings(Id:string):Promise<any>
+    canceledBookings(Id: string): Promise<any>
+    upBookings(Id: string,status:string): Promise<any>
+    hostCancelBookings(id:string):Promise<any>
     getBookingById(Id:string,bookingId:string):Promise<any>
     updateBookingStatus(bookingId: string, newstatus:string):Promise<any>
     getBookingdate(id:string):Promise<any>
