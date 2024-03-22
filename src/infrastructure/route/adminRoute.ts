257de037147623778aa96e44c4ac9b66f8d4664f
NeeharPrem <neeharprem94@gmail.com>
2024-03-22 20:53:22 +0530

    feat: new routes added to the admin route
    - routes for getting data for admin dashboard
    - route  for setting new service fee

--- src/infrastructure/route/adminRoute.ts
+++ src/infrastructure/route/adminRoute.ts
@@ -2,15 +2,18 @@ import AmenityController from "../../adapter/amenitesController";
 import AdminController from "../../adapter/adminController";
 import BookingController from "../../adapter/bookingController";
 import CategoryController from "../../adapter/categoryController";
+import paymentController from "../../adapter/paymentController";
 import AdminUsercases from "../../use_case/adminUsecases";
 import AmenitiesUsecase from "../../use_case/amenitiesUsecase";
 import BookingUsecase from "../../use_case/bookingUsecase";
 import CategoryUsecase from "../../use_case/categoryUsecase";
+import paymentUsecase from "../../use_case/paymentUsecase";
 import AdminRepository from "../repository/adminRepository";
 import AmenityRepository from "../repository/amenityRepository";
 import BookingRepository from "../repository/bookingRepository";
 import CategoryRepository from "../repository/categoryRepository";
 import HostRepository from "../repository/HostRepository";
+import paymensettingRepository from "../repository/paymensettingRepository";
 import UserRepository from "../repository/userRepository";
 import Encrypt from "../passwordRepository/hashpassword";
 import express from "express";
@@ -28,16 +31,20 @@ const bookingRepository= new BookingRepository()
 const categoryRepository=new CategoryRepository();
 const hostRepository= new HostRepository ();
 const paymentRepository= new PaymentRepository();
+const paymensettingrepository = new paymensettingRepository()
 const userRepository = new UserRepository();
 
 const admiUseCase = new AdminUsercases(encrypt, userRepository, jwtToken, adminRepository, hostRepository);
 const amenityUsecase = new AmenitiesUsecase(amenityRepository);
+const bookingUsecase = new BookingUsecase(bookingRepository, paymentRepository, paymensettingrepository)
 const categoryUsecase=new CategoryUsecase(categoryRepository)
-const bookingUsecase = new BookingUsecase(bookingRepository, paymentRepository)
+const paymentusecase = new paymentUsecase(paymensettingrepository);
+
 const controller = new AdminController(admiUseCase);
 const amenityController = new AmenityController(amenityUsecase);
 const categoryController= new CategoryController(categoryUsecase)
 const bookingController = new BookingController(bookingUsecase)
+const paymentcontroller = new paymentController(paymentusecase)
 
 const router = express.Router();
 
@@ -61,6 +68,12 @@ router.put('/category', (req, res) => categoryController.newEntry(req, res));
 router.patch("/category", (req, res) => categoryController.deleteEntry(req, res))
 router.post("/category", (req, res) => categoryController.editEntry(req, res))
 
+// admin dashboard
+router.get('/adminDashboard', (req, res) => bookingController.adminDashboard(req,res))
+
 // bookings
 router.get('/bookings',(req,res)=>bookingController.allbookings(req,res))
+
+//payment setting
+router.post('/payments', (req,res) => paymentcontroller.addServiceFee(req,res))
 export default router;
