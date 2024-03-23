f6bfda0cb76e1488d213815a20f72fdccb948ed7
NeeharPrem <neeharprem94@gmail.com>
2024-03-23 09:45:17 +0530

    feat: new changes implimented to imporve booking logic
    - admin service charge is calculated and stored in the booking
    - booking cancel logic updated. refund type is stored in the booking db
    - chargeId will be saved in the booking db

--- src/use_case/bookingUsecase.ts
+++ src/use_case/bookingUsecase.ts
@@ -1,19 +1,23 @@
 import IBookingRepo from "./interface/bookingRepoInterface";
 import PaymentRepository from "../infrastructure/repository/paymentRepository";
 import Booking from "../domain/booking";
+import IPaymentset from "./interface/paymentRepoInterface";
 
 class BookingUsecase{
     private IBooking:IBookingRepo
     private PaymentRepo: PaymentRepository
+    private IPaymentset: IPaymentset
     constructor(
         IBooking:IBookingRepo,
-        PaymentRepo: PaymentRepository
+        PaymentRepo: PaymentRepository,
+        IPaymentset: IPaymentset
     ){
         this.IBooking= IBooking;
         this.PaymentRepo = PaymentRepo
+        this.IPaymentset = IPaymentset
     }
 
-    async addBooking(Data: Booking, userId: string) {
+    async addBooking(Data: Booking, userId: string, chargeId:string) {
         const convertToISO = (dateString: string) => {
             const parts = dateString.split('/');
             const day = parts[0];
@@ -27,7 +31,22 @@ class BookingUsecase{
         };
 
         try {
-            console.log(Data.checkInDate, '', Data.checkOutDate);
+            const setOut = await this.IPaymentset.LatestFee();
+            let serviceFeePercentage = setOut.serviceFee
+            const defaultServiceFeePercentage = 10;
+            serviceFeePercentage = (typeof serviceFeePercentage === 'number' && !isNaN(serviceFeePercentage)) ? serviceFeePercentage : defaultServiceFeePercentage;
+
+            let newTotal = Data?.totalAmount
+            if (isNaN(newTotal)) {
+                console.error('Rent conversion error, received:', Data?.totalAmount);
+            }
+
+            const serviceFeeCalculated = !isNaN(serviceFeePercentage) ? (newTotal * serviceFeePercentage / 100) : newTotal;
+            if (isNaN(serviceFeeCalculated)) {
+                console.error('Calculated rent resulted in NaN. Rent:', serviceFeeCalculated, 'Service Fee Percentage:', serviceFeePercentage);
+            }
+             
+            
             const checkInDateISO = convertToISO(Data.checkInDate);
             const checkOutDateISO = convertToISO(Data.checkOutDate);
 
@@ -41,9 +60,10 @@ class BookingUsecase{
                 checkOutDate: checkOutDateISO,
                 isCancelled: false,
                 totalAmount: Data?.totalAmount,
-                status: 'pending'
+                status: 'pending',
+                chargeId: chargeId,
+                serviceFee: serviceFeeCalculated
             };
-
             const saveData = await this.IBooking.addnewBooking(booking);
             return saveData;
         } catch (error) {
@@ -68,7 +88,6 @@ class BookingUsecase{
     async PaymentConfirm(data: any) {
         const paymentSuccess = await this.PaymentRepo.PaymentSuccess(data)
         if (!paymentSuccess) {
-            console.log("fail");
             return null
         } else {
             return true
@@ -78,7 +97,6 @@ class BookingUsecase{
     async getBookings(id: string) {
         try {
             const Data = await this.IBooking.getBookings(id)
-            console.log(Data)
             if (Data.length>0) {
                 return {
                     status: 200,
@@ -122,7 +140,6 @@ class BookingUsecase{
     async upBookings(id: string,status:string) {
     try {
         const Data = await this.IBooking.upBookings(id,status)
-        console.log(Data,'upb')
         if (Data) {
             return {
                 status: 200,
@@ -151,28 +168,51 @@ class BookingUsecase{
 
     async hostCancelBookings(id: string) {
         try {
-            const Data = await this.IBooking.hostCancelBookings(id)
+            const Data = await this.IBooking.checkBookingById(id);
             if (Data) {
-                return {
-                    status: 200,
-                    data: Data
-                }
-            } else {
-                return {
-                    status: 400,
-                    data: {
-                        message: "No Bookings"
+                let RefundCents=0;
+                if (Data.cancelReq === true) {
+                    if (Data.refundType === 'full') {
+                        RefundCents = (Data.totalAmount + Data.serviceFee) * 100;
+                    } else if (Data.refundType === 'partial') {
+                        RefundCents = Data.totalAmount * 100;
                     }
+                } else {
+                    RefundCents = (Data.totalAmount + Data.serviceFee) * 100;
+                }
+                RefundCents = Math.round(RefundCents);
+                const refundOut = await this.PaymentRepo.createRefund(Data.chargeId, RefundCents);
+                const RefundData = await this.IBooking.hostupdateBookingStatus(id, refundOut?.id ?? '');
+                if (RefundData) {
+                    return { status: 200, data: Data };
+                } else {
+                    return { status: 400, data: { message: "No Bookings" } };
                 }
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 
     async cancelBooking(Id: string,bookingId: string) {
         try {
-            console.log('useca')
+            const datDate = await this.IBooking.createdTime(Id, bookingId)
+            const output = checkTimeDifferenceAndConvertTo12Hr(datDate.createdAt)
+
+            function checkTimeDifferenceAndConvertTo12Hr(timestamp:any) {
+                const date = new Date(timestamp);
+                const currentDate = new Date();
+                const diffHours = Math.abs(date.getTime() - currentDate.getTime()) / 36e5;
+                const isGreaterThan48Hours = diffHours > 48;
+                let hours = date.getHours();
+                const minutes = date.getMinutes();
+                const ampm = hours >= 12 ? 'PM' : 'AM';
+                hours = hours % 12;
+                hours = hours ? hours : 12;
+                const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+                return isGreaterThan48Hours
+            }
+             
             const booking = await this.IBooking.getBookingById(Id,bookingId);
             if (!booking) {
                 return {
@@ -180,8 +220,13 @@ class BookingUsecase{
                     data: { message: "Booking not found or does not belong to the user" },
                 };
             }
-            const newstatus= 'cancelled'
-            await this.IBooking.updateBookingStatus(bookingId, newstatus);
+            if(output){
+                const refundType='partial'
+                await this.IBooking.updateBookingStatus(bookingId,refundType);
+            }else{
+                const refundType = 'full'
+                await this.IBooking.updateBookingStatus(bookingId,refundType);
+            }
             return {
                 status: 200,
                 data: { message: "Booking canceled successfully" },
@@ -267,6 +312,64 @@ class BookingUsecase{
             }
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    // host dashboard
+    async dashboard(id:string) {
+        try {
+            const Data = await this.IBooking.dashboard(id)
+            if (Data) {
+                return {
+                    status: 200,
+                    data: Data
+                };
+            } else {
+                return {
+                    status: 200,
+                    data: {
+                        message: "No bookings available.",
+                        isEmpty: true
+                    }
+                };
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                status: 500,
+                data: {
+                    message: "An error occurred while fetching bookings."
+                }
+            };
+        }
+    }
+
+    // admin dashboard
+    async adminDashboard() {
+        try {
+            const Data = await this.IBooking.adminDashboard()
+            if (Data) {
+                return {
+                    status: 200,
+                    data: Data
+                };
+            } else {
+                return {
+                    status: 200,
+                    data: {
+                        message: "No bookings available.",
+                        isEmpty: true
+                    }
+                };
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                status: 500,
+                data: {
+                    message: "An error occurred while fetching bookings."
+                }
+            };
         }
     }
 }
