f7eb52de37a58c99945822231269abf1dba181b9
NeeharPrem <neeharprem94@gmail.com>
2024-03-23 11:08:39 +0530

    Refined Booking Repository Interface with Enhanced Methods
    Enhancements and Modifications:
    - Added `hostCancelBookings` method for host-initiated booking cancellations.
    - Introduced `hostupdateBookingStatus` for updating booking statuses by the host, including refund ID parameter for transaction tracking.
    - Improved `createdTime` method to include roomId for specific booking time queries.
    - Extended `getBookingById` to support dual identification via User ID and Booking ID for precise retrieval.
    - Optimized `checkBookingById` for more efficient booking existence checks.
    - Upgraded `updateBookingStatus` with a refundType parameter to handle different refund scenarios.
    - Enriched `getBookingdate` for retrieving booking dates, enhancing date-related operations.
    - Added `checkDateAvailability` for better handling of room availability queries based on check-in and check-out dates.
    - Introduced `bookingAndreview` method to correlate bookings with reviews for a given room and user.
    - Implemented `hostReviewcheck` for hosts to verify reviews based on host and user IDs.
    - Expanded `dashboard` functionality to provide more comprehensive data retrieval based on user ID.
    - Developed `adminDashboard` for a holistic view of platform metrics and statistics for administrative purposes.
    
    This commit encompasses crucial updates to the IBookingRepo interface, aimed at refining the booking process, improving data retrieval mechanisms, and enhancing the overall functionality for users, hosts, and administrators.

--- src/use_case/interface/bookingRepoInterface.ts
+++ src/use_case/interface/bookingRepoInterface.ts
@@ -7,10 +7,17 @@ interface IBookingRepo{
     canceledBookings(Id: string): Promise<any>
     upBookings(Id: string,status:string): Promise<any>
     hostCancelBookings(id:string):Promise<any>
+    hostupdateBookingStatus(id: string,refundId:string):Promise<any>
+    createdTime(id:string,roomId:string):Promise<any>
     getBookingById(Id:string,bookingId:string):Promise<any>
-    updateBookingStatus(bookingId: string, newstatus:string):Promise<any>
+    checkBookingById(Id: string): Promise<any>
+    updateBookingStatus(bookingId: string,refundType:string):Promise<any>
     getBookingdate(id:string):Promise<any>
     checkDateAvailability(roomId: string, checkInISO: string, checkOutISO:string):Promise<any>
+    bookingAndreview(roomId:string,Id:string):Promise<any>
+    hostReviewcheck(hostId:string,Id:string):Promise<any>
+    dashboard(id:string):Promise<any>
+    adminDashboard():Promise<any>
 }
 
 export default IBookingRepo
