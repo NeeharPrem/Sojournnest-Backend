c98d7ca0c7d0bcec76f504acb75dea2e980c9d2e
NeeharPrem <neeharprem94@gmail.com>
2024-04-06 21:00:11 +0530

    feat: interface for updating or adding the fcm token to the user db

--- src/use_case/interface/userRepo.ts
+++ src/use_case/interface/userRepo.ts
@@ -5,6 +5,7 @@ interface IUserRepo {
     findByEmail(email: string): Promise<User | null>;
     findById(_id: string): Promise<User | null>;
     findOneAndUpdate(query: any, update: any, options?: any): Promise<User | null>;
+    saveFcmtoken(id:string,fcmtoken:string):Promise<any>
     updateRefreshToken(_id: string, refreshToken: string): Promise<User | null>;
     findAndUpdate(user: User): Promise<any>
 }
