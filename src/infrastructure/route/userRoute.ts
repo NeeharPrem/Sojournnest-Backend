3436a69c03c56d4e0e1cf7fcd84d8181ed4b19b7
NeeharPrem <neeharprem94@gmail.com>
2024-04-07 10:44:56 +0530

    feat: Added route protection for user route
    - route protection will prevent the unauthrized access to the route

--- src/infrastructure/route/userRoute.ts
+++ src/infrastructure/route/userRoute.ts
@@ -80,74 +80,74 @@ const router=express.Router();
 router.post("/", (req, res) => controller.signup(req, res));
 router.post("/verify-otp", (req, res) => controller.verifyotp(req,res))
 router.post("/resend-otp", (req, res) => controller.resendOtp(req, res))
-router.get("/",(req, res) => controller.profile(req, res))
+router.get("/", protect,(req, res) => controller.profile(req, res))
 router.patch("/:id", protect, ImageUpload.single('avatar'),(req,res)=>controller.updateProfile(req,res))
-router.put("/:id",ImageUpload.single('verifyId'), (req, res) => controller.uploadId(req, res))
+router.put("/:id", protect,ImageUpload.single('verifyId'), (req, res) => controller.uploadId(req, res))
 router.get('/listings/:id',(req, res) => hostcontroller.roomDetail(req, res))
-router.post('/fcm',(req,res)=>controller.saveFcmtoken(req,res))
+router.post('/fcm', protect,(req,res)=>controller.saveFcmtoken(req,res))
 
 // host 
-router.get('/host/listings', (req,res)=>hostcontroller.getListings(req,res))
-router.put('/host/listings', ImageUpload.array('images'),(req,res)=>hostcontroller.addRoom(req,res))
-router.get('/host/listings/:id', (req, res) => hostcontroller.roomData(req, res))
-router.patch('/host/listings/:id', (req,res)=>hostcontroller.unlist(req,res))
-router.put('/host/listings/:id', ImageUpload.array('images'), (req, res) => hostcontroller.roomDataUpdate(req, res))
+router.get('/host/listings', protect, (req,res)=>hostcontroller.getListings(req,res))
+router.put('/host/listings', protect, ImageUpload.array('images'),(req,res)=>hostcontroller.addRoom(req,res))
+router.get('/host/listings/:id', protect, (req, res) => hostcontroller.roomData(req, res))
+router.patch('/host/listings/:id', protect, (req,res)=>hostcontroller.unlist(req,res))
+router.put('/host/listings/:id', protect, ImageUpload.array('images'), (req, res) => hostcontroller.roomDataUpdate(req, res))
 
 // getting the listing data
-router.get('/listings', (req, res) => hostcontroller.allListings(req, res));
+router.get('/listings', protect, (req, res) => hostcontroller.allListings(req, res));
 
 // host chat
-router.post('/host/chat',(req, res) => hostcontroller.newConversation(req, res))
-router.get('/host/chat/:id',(req, res) => hostcontroller.getConversations(req, res))
-router.post('/host/chat/messages',(req,res)=>hostcontroller.addMessage(req,res))
-router.get('/host/chat/messages/:id',(req,res)=>hostcontroller.getMessages(req,res))
+router.post('/host/chat', protect,(req, res) => hostcontroller.newConversation(req, res))
+router.get('/host/chat/:id', protect,(req, res) => hostcontroller.getConversations(req, res))
+router.post('/host/chat/messages', protect,(req,res)=>hostcontroller.addMessage(req,res))
+router.get('/host/chat/messages/:id', protect,(req,res)=>hostcontroller.getMessages(req,res))
 
 
 //room booking
-router.put('/bookings',(req,res)=>bookingcontroller.newBooking(req,res))
-router.get('/bookings',(req,res)=>bookingcontroller.getBookings(req,res))
-router.get('/bookings/cancelled', (req, res) => bookingcontroller.canceledBookings(req, res))
-router.patch('/bookings/:id',(req, res) => bookingcontroller.cancelBooking(req, res))
-router.get('/bookings/:id',(req,res)=>bookingcontroller.getBookingdate(req,res))
-router.post('/bookings/check-availability', (req, res) => bookingcontroller.checkDateAvailability(req,res))
+router.put('/bookings',protect,(req,res)=>bookingcontroller.newBooking(req,res))
+router.get('/bookings', protect,(req,res)=>bookingcontroller.getBookings(req,res))
+router.get('/bookings/cancelled', protect,(req, res) => bookingcontroller.canceledBookings(req, res))
+router.patch('/bookings/:id', protect,(req, res) => bookingcontroller.cancelBooking(req, res))
+router.get('/bookings/:id', protect,(req,res)=>bookingcontroller.getBookingdate(req,res))
+router.post('/bookings/check-availability', protect,(req, res) => bookingcontroller.checkDateAvailability(req,res))
 router.post('/bookings',(req, res) => bookingcontroller.payment(req, res))
 router.post('/webhook', (req, res) => bookingcontroller.webhook(req, res))
 
 //host managing booking
-router.get('/host/bookings',(req,res)=>bookingcontroller.upBookings(req,res))
-router.patch('/host/bookings/:id', (req, res) => bookingcontroller.hostCancelBookings(req, res))
+router.get('/host/bookings', protect,(req,res)=>bookingcontroller.upBookings(req,res))
+router.patch('/host/bookings/:id', protect,(req, res) => bookingcontroller.hostCancelBookings(req, res))
 
 //room Date blocking
-router.put('/host/managedate/:id',(req,res) => hostcontroller.blockDate(req,res))
-router.get('/host/managedate/:id', (req,res)=>hostcontroller.blockedDates(req,res))
-router.patch('/host/managedate/:id',(req,res)=>hostcontroller.removeDate(req,res))
+router.put('/host/managedate/:id', protect,(req,res) => hostcontroller.blockDate(req,res))
+router.get('/host/managedate/:id', protect, (req,res)=>hostcontroller.blockedDates(req,res))
+router.patch('/host/managedate/:id', protect,(req,res)=>hostcontroller.removeDate(req,res))
 
 //wishlist
-router.put('/wishlist/:id', (req, res) => whishlistcontroller.addTowishlist(req,res))
-router.get('/wishlist',(req, res) => whishlistcontroller.userWishlists(req, res))
-router.get('/wishlist/:id', (req, res) => whishlistcontroller.checkExisist(req, res))
-router.patch('/wishlist/:id', (req, res) => whishlistcontroller.removeWishlist(req, res))
+router.put('/wishlist/:id', protect,(req, res) => whishlistcontroller.addTowishlist(req,res))
+router.get('/wishlist', protect,(req, res) => whishlistcontroller.userWishlists(req, res))
+router.get('/wishlist/:id', protect,(req, res) => whishlistcontroller.checkExisist(req, res))
+router.patch('/wishlist/:id', protect,(req, res) => whishlistcontroller.removeWishlist(req, res))
 
 // get amenities and category
-router.get('/amenities', (req, res) => amenityController.findAmenity(req, res));
+router.get('/amenities',(req, res) => amenityController.findAmenity(req, res));
 router.get('/category', (req, res) => categoryController.findCategory(req, res));
 
 //rating
-router.post('/rating/:id', (req, res) => reviewcontroller.addReview(req,res))
-router.patch('/rating/:id', (req, res) => reviewcontroller.roomReviewEdit(req,res))
+router.post('/rating/:id', protect,(req, res) => reviewcontroller.addReview(req,res))
+router.patch('/rating/:id', protect,(req, res) => reviewcontroller.roomReviewEdit(req,res))
 router.get('/rating/:id',(req,res)=>reviewcontroller.getRoomReviews(req,res))
 router.get('/rating',(req,res)=>reviewcontroller.bookingAndreview(req,res))
 
 // host review
 router.get('/host/rating', (req, res) => reviewcontroller.hostReviewcheck(req, res))
-router.post('/host/rating/:id', (req, res) => reviewcontroller.postHostreview(req, res))
-router.patch('/host/rating/:id', (req, res) => reviewcontroller.HostReviewEdit(req, res))
+router.post('/host/rating/:id', protect,(req, res) => reviewcontroller.postHostreview(req, res))
+router.patch('/host/rating/:id', protect,(req, res) => reviewcontroller.HostReviewEdit(req, res))
 router.get('/host/rating/:id', (req, res) => reviewcontroller.getHostReviews(req, res))
 
 //hostdashboard
 router.get('/host/hostdashboard', (req, res) => bookingcontroller.dashboard(req,res))
 
 // get user
-router.get("/:id", (req, res) => controller.getUser(req, res))
+router.get("/:id",(req, res) => controller.getUser(req, res))
 
 export default router
