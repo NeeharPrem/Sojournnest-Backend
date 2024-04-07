2cfe42a7e50acf76e28c86d7e17970e71fdefac1
NeeharPrem <neeharprem94@gmail.com>
2024-04-08 01:28:20 +0530

    feat(booking interface): new interfaces added
    - interface for booking confirm added
    - interface for bookingbyId added
    - proper semi colon added in the code

--- src/use_case/interface/bookingRepoInterface.ts
+++ src/use_case/interface/bookingRepoInterface.ts
@@ -1,23 +1,25 @@
 import Booking from "../../domain/booking"
 
 interface IBookingRepo{
-    addnewBooking(Data: any):Promise<any>
+    addnewBooking(Data: any):Promise<any>;
     allbookings(page:number,limit:number):Promise<any>;
-    getBookings(Id:string):Promise<any>
-    canceledBookings(Id: string): Promise<any>
-    upBookings(Id: string,status:string): Promise<any>
-    hostCancelBookings(id:string):Promise<any>
-    hostupdateBookingStatus(id: string,refundId:string):Promise<any>
-    createdTime(id:string,roomId:string):Promise<any>
-    getBookingById(Id:string,bookingId:string):Promise<any>
-    checkBookingById(Id: string): Promise<any>
-    updateBookingStatus(bookingId: string,refundType:string):Promise<any>
-    getBookingdate(id:string):Promise<any>
-    checkDateAvailability(roomId: string, checkInISO: string, checkOutISO:string):Promise<any>
-    bookingAndreview(roomId:string,Id:string):Promise<any>
-    hostReviewcheck(hostId:string,Id:string):Promise<any>
-    dashboard(id:string):Promise<any>
-    adminDashboard():Promise<any>
+    getBookings(Id:string):Promise<any>;
+    hostConfirmBooking(Id:string):Promise<any>;
+    canceledBookings(Id: string): Promise<any>;
+    upBookings(Id: string,status:string): Promise<any>;
+    hostCancelBookings(id:string):Promise<any>;
+    hostupdateBookingStatus(id: string,refundId:string,pass:string):Promise<any>;
+    createdTime(id:string,roomId:string):Promise<any>;
+    getBookingById(Id:string,bookingId:string):Promise<any>;
+    BookingById(Id:string):Promise<any>;
+    checkBookingById(Id: string): Promise<any>;
+    updateBookingStatus(bookingId: string,refundType:string):Promise<any>;
+    getBookingdate(id:string):Promise<any>;
+    checkDateAvailability(roomId: string, checkInISO: string, checkOutISO:string):Promise<any>;
+    bookingAndreview(roomId:string,Id:string):Promise<any>;
+    hostReviewcheck(hostId:string,Id:string):Promise<any>;
+    dashboard(id:string):Promise<any>;
+    adminDashboard():Promise<any>;
 }
 
 export default IBookingRepo
