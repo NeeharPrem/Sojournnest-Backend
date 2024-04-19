a547d4711c1fb01a4c453a4c3b88d3f7cdcfab83
NeeharPrem <neeharprem94@gmail.com>
2024-04-19 09:46:33 +0530

    feat(connectDB): get mongoDB connect string from .env

--- src/infrastructure/config/connectDB.ts
+++ src/infrastructure/config/connectDB.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB=async()=>{
     try{
-        const mongo_uri="mongodb+srv://x2cvicious123:n3rLzUNO8oyCXZBE@cluster0.iftaqh1.mongodb.net/bookaday?retryWrites=true&w=majority";
+        const mongo_uri = process.env.MONGO_URI;
         if(mongo_uri){
             await mongoose.connect(mongo_uri)
             console.log("Connected to database")
